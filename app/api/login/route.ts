62c26ecedd6e8690d65646a4644763b6ce316f60
AutistiCoder <autisticoder@gmail.com>
2026-04-13 10:55:51 -0700

    added correct labelling, password hashing

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,12 +1,28 @@
 // log the user in using POST
 import { NextResponse } from "next/server";
-
+import bcrypt from "bcryptjs";
 export async function POST(req: Request) {
   const formData = await req.formData();
 
   const email = formData.get("email");
   const password = formData.get("password");
   console.log(email,password);
-  // Fake success for now
- return NextResponse.redirect(new URL("/?loggedIn=true", req.url));
+  if (email === null)
+    return NextResponse.json({error: "missing email"}, {status: 400});
+  if (password === null)
+    return NextResponse.json({error: "missing password"}, {status: 400});
+  const salt = 1; // for greater security, don't hardcode like I am doing here - this is for demo purposes only
+  try
+  {
+    const hashString = await bcrypt.hash(password as string,salt);
+    console.log(hashString);
+    return NextResponse.redirect(new URL("/?loggedIn=true", req.url));
+  }
+  catch (e)
+  {
+    console.log(e);
+    return NextResponse.json({error: ""}, {status: 500}); // error response should be blank without more info about the error, so as not to accidentally expose server information
+  }
+  
+  
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,8 @@
 export default function LoginComponent()
 {
  return <form action="/api/login" method="POST">
-  <input name="email" />
-  <input name="password" type="password" />
+  Username: <input name="email" />
+  <br />Password: <input name="password" type="password" /><br/>
   <button type="submit">Login</button>
 </form>
 }
